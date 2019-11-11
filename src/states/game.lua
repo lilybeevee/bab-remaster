ec50c674f3d528b7d89c60b9853355163dbb3c4a
vitellaryjr <vitellaryjr@gmail.com>
2019-11-11 11:17:14 -0700

    diagonalss

--- src/states/game.lua
+++ src/states/game.lua
@@ -14,14 +14,24 @@ function game:draw()
 end
 
 function game:keypressed(key)
-  if key == "w" or key == "up" then
-    self:doTurn(0, -1)
-  elseif key == "s" or key == "down" then
-    self:doTurn(0, 1)
-  elseif key == "a" or key == "left" then
-    self:doTurn(-1, 0)
-  elseif key == "d" or key == "right" then
+  if key == "d" or key == "right" or key == "kp6" then
     self:doTurn(1, 0)
+  elseif key == "kp3" then
+    self:doTurn(1, 1)
+  elseif key == "s" or key == "down" or key == "kp2" then
+    self:doTurn(0, 1)
+  elseif key == "kp1" then
+    self:doTurn(-1, 1)
+  elseif key == "a" or key == "left" or key == "kp4" then
+    self:doTurn(-1, 0)
+  elseif key == "kp7" then
+    self:doTurn(-1, -1)
+  elseif key == "w" or key == "up" or key == "kp8" then
+    self:doTurn(0, -1)
+  elseif key == "kp9" then
+    self:doTurn(1, -1)
+  elseif key == "space" then
+    self:doTurn(0, 0)
   end
 end
 
